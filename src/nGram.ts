84915343fef0a47cb2ae081b0f12d363bb0f7156
Vinícius Smanioto <smaniotov@gmail.com>
2021-09-11 18:40:55 -0300

    initial commit

---
+++ src/nGram.ts
@@ -0,0 +1,17 @@
+const nGram = (text: string, separator: string = ' ') => {
+  const normalizedText = text.replace(/[.,!?:]/gi, '');
+
+  const list: string[] = normalizedText.split(separator);
+
+  return list.reduce((out: string[], _: any, index: number) => {
+    const currentInteration = list.slice(index);
+
+    const newValues = currentInteration.map((_1, interationIndex) =>
+      currentInteration.slice(0, interationIndex + 1).join(' ')
+    );
+
+    return [...out, ...newValues];
+  }, []);
+};
+
+export default nGram;
---
+++ test/nGram.spec.ts
@@ -0,0 +1,32 @@
+import nGram from '../src/nGram';
+
+describe('nGram', () => {
+  it.each([
+    ['Showmethecode.', ['Showmethecode'], ' '],
+    ['Show methecode.', ['Show','Show methecode', 'methecode'], ' '],
+    [
+      'Show me the code.',
+      [
+        'Show',
+        'Show me',
+        'Show me the',
+        'Show me the code',
+        'me',
+        'me the',
+        'me the code',
+        'the',
+        'the code',
+        'code'
+      ],
+      ' '
+    ],
+    ['Cys-Gly', ['Cys', 'Cys Gly', 'Gly'], '-']
+  ])(
+    'should convert the text "%s" into "%o"',
+    (text: any, expected: any, separator) => {
+      const result = nGram(text, separator);
+
+      expect(result).toMatchObject(expected);
+    }
+  );
+});
